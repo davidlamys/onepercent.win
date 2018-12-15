3bef5e34fd65eed1f90a2c2d9c9132b6019f3289
David Lam <david.lys@gmail.com>
2018-12-15 14:03:49 +0800

    Add offline capability

--- OnePercentWin/AppDelegate.swift
+++ OnePercentWin/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseFirestore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        let settings = FirestoreSettings()
+        settings.isPersistenceEnabled = true
+        let db = Firestore.firestore()
+        db.settings = settings
+
         return true
     }
     
