b4b084d7dea7daf8e1fcb32a4ce91ddabc9d1a62
David Lam <david.lys@gmail.com>
2019-01-21 11:53:09 +0800

    Add fonts and themehelper

--- OnePercentWin/SharedServices/ThemeHelper.swift
+++ OnePercentWin/SharedServices/ThemeHelper.swift
@@ -8,8 +8,52 @@
 
 import UIKit
 
+enum SizeType {
+    case large
+    case medium
+    case small
+    
+    var fontSize: CGFloat {
+        switch self {
+        case .large: return 21.0
+        case .medium: return 18.0
+        case .small: return 12.0
+        }
+    }
+}
+
 class ThemeHelper {
-    static func defaultFont(size: CGFloat) -> UIFont {
+
+    static func defaultFont(fontSize: SizeType) -> UIFont {
+        return UIFont(name: "Roboto-Regular", size: fontSize.fontSize)!
+//            ?? UIFont.systemFont(ofSize: fontSize.fontSize)
+    }
+
+    private static func defaultFont(size: CGFloat) -> UIFont {
         return UIFont(name: "Roboto-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    static func defaultOrange() -> UIColor {
+        return UIColor(rgb: 0xFF9500)
+    }
+    
+
+}
+
+fileprivate extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
     }
 }
