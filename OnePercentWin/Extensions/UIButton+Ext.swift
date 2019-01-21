019b7332d71ff20f783a92e4bde82331ba4fbe79
David Lam <david.lys@gmail.com>
2019-01-21 15:39:33 +0800

    Add uibutton extension to style buttons

---
+++ OnePercentWin/Extensions/UIButton+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  UIButton+Ext.swift
+//  OnePercentWin
+//
+//  Created by David on 21/1/19.
+//  Copyright © 2019 David Lam. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func applyStyle() {
+        self.backgroundColor = ThemeHelper.defaultOrange()
+        self.setTitleColor(.black, for: .normal)
+        self.layer.cornerRadius = 5.0
+        self.clipsToBounds = true
+    }
+}
