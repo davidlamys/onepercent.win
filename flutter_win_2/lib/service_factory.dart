330f7421916e9fe8ad9cacb24ea16ccb98abb1d8
David Lam <davidlys@gmail.com>
2020-09-03 08:39:46 +0800

    Fix fake goal factory

--- flutter_win_2/lib/service_factory.dart
+++ flutter_win_2/lib/service_factory.dart
@@ -2,7 +2,7 @@ import 'package:flutter_win_2/Services/user_service.dart';
 
 import 'Services/goal_service.dart';
 
-final useFake = false;
+final useFake = true;
 
 class ServiceFactory {
   static FakeGoalService fakeGoalService = FakeGoalService();
