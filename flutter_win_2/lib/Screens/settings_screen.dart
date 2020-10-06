8e623a6d24ab917fd275471561cc674ed0a60bd6
David Lam <davidlys@gmail.com>
2020-10-06 15:42:52 +0800

    Use settings bloc in settings screen

--- flutter_win_2/lib/Screens/settings_screen.dart
+++ flutter_win_2/lib/Screens/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_win_2/Services/user_service.dart';
 import 'package:flutter_win_2/Styling/colors.dart';
+import 'package:flutter_win_2/blocs/settings_provider.dart';
 
 class SettingsScreen extends StatelessWidget {
   static const id = "settingsScreen";
@@ -9,6 +10,7 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final settingsBloc = SettingsProvider.of(context).bloc;
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
@@ -23,9 +25,7 @@ class SettingsScreen extends StatelessWidget {
                 color: appRed,
                 child: Text('Log out'),
                 onPressed: () {
-                  UserService().signOutUser().then((value) {
-                    Navigator.of(context).popUntil((route) => route.isFirst);
-                  });
+                  settingsBloc.logoutUser();
                 },
               ),
             ),
