23d949be412168a40bd004b3a432c8a35ddb1c27
David Lam <davidlys@gmail.com>
2020-10-12 12:46:46 +0800

    ios implementation

--- flutter_win_2/ios/Runner/AppDelegate.swift
+++ flutter_win_2/ios/Runner/AppDelegate.swift
@@ -3,11 +3,29 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+        }
+        if let controller = self.window.rootViewController as? FlutterViewController  {
+            let batteryChannel = FlutterMethodChannel(name: "dexterx.dev/flutter_local_notifications_example",
+                                                      binaryMessenger: controller.binaryMessenger)
+            batteryChannel.setMethodCallHandler({
+                (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+                // Note: this method is invoked on the UI thread.
+                // Handle battery messages.
+                if (call.method == "getBatteryLevel") {
+                    let tz = TimeZone.current
+                    result(tz.identifier)
+                }
+            })
+        }
+                
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
