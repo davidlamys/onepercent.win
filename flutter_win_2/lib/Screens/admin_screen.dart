e453053679ad908bae91ba279726d6dfea88a9dc
David Lam <davidlys@gmail.com>
2020-11-15 11:56:58 +0800

    extract goal list tile from admin screen as its own widget

--- flutter_win_2/lib/Screens/admin_screen.dart
+++ flutter_win_2/lib/Screens/admin_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_win_2/Model/record.dart';
+import 'package:flutter_win_2/Widgets/goal_list_tile.dart';
 import 'package:flutter_win_2/blocs/index.dart';
-import 'package:intl/intl.dart';
 
 class AdminScreen extends StatelessWidget {
   const AdminScreen({Key key}) : super(key: key);
@@ -29,62 +29,15 @@ class AdminScreen extends StatelessWidget {
             return ListView.builder(
                 scrollDirection: Axis.vertical,
                 itemBuilder: (BuildContext context, int index) {
-                  return _buildDateSelectionBox(context, goals[index]);
+                  final listTileAdapter =
+                      AdminScreenListTileAdapter(goals[index]);
+                  return GoalListTile(
+                    adapter: listTileAdapter,
+                  );
                 },
                 reverse: false,
                 itemCount: goals.length);
           }),
     );
-  }
-
-  Widget _buildDateSelectionBox(BuildContext context, Record record) {
-    final timeStampString = dayFormat().format(record.timestamp);
-    final token1 = "I want to ";
-    final token2 = " because it is going to help me to ";
-    final statusEmoji = getEmojiString(record);
-    if (record.status == null) {}
-    final titleString = record.createdBy +
-        ": " +
-        token1 +
-        record.name +
-        token2 +
-        record.reason +
-        statusEmoji;
-
-    return ListTile(
-        contentPadding: EdgeInsets.symmetric(
-          vertical: 8.0,
-          horizontal: 16.0,
-        ),
-        title: Text(
-          titleString,
-        ),
-        subtitle: Text(
-          timeStampString,
-        ),
-        onTap: () {
-          showFullRecord(context, record, "Reflection");
-        });
-  }
-
-  void showFullRecord(BuildContext context, Record record, String titleString) {
-    if (record.notes == null) {
-      showDialog(
-          context: context,
-          child: new AlertDialog(
-            title: new Text("No reflection yet"),
-          ));
-      return;
-    }
-    showDialog(
-        context: context,
-        child: new AlertDialog(
-          title: new Text(titleString),
-          content: new Text(record.notes),
-        ));
-  }
-
-  DateFormat dayFormat() {
-    return DateFormat('d MMM yy');
   }
 }
