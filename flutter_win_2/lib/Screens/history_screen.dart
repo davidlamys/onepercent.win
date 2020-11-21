42e48078f2e380cb42df221bb1bff1bebc9aca89
David Lam <davidlys@gmail.com>
2020-11-22 00:17:00 +0800

    add rough markers

--- flutter_win_2/lib/Screens/history_screen.dart
+++ flutter_win_2/lib/Screens/history_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_win_2/Styling/colors.dart';
-import 'package:flutter_win_2/Widgets/app_button.dart';
+import 'package:flutter_win_2/Model/record.dart';
 import 'package:flutter_win_2/Widgets/history_timeline_list.dart';
 import 'package:flutter_win_2/blocs/index.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -27,20 +26,7 @@ class HistoryScreen extends StatelessWidget {
 
             return Column(
               children: [
-                TableCalendar(
-                  calendarController: _calendarController,
-                  onVisibleDaysChanged: (DateTime startDate, DateTime endDate,
-                      CalendarFormat format) {
-                    bloc.onVisibleDaysChanged(startDate, endDate);
-                  },
-                  calendarStyle: CalendarStyle(
-                    selectedColor: Colors.deepOrange[400],
-                    todayColor: Colors.deepOrange[200],
-                    markersColor: Colors.brown[700],
-                    outsideDaysVisible: false,
-                  ),
-                  endDay: DateTime.now(),
-                ),
+                buildTableCalendar(bloc, recordsForRange),
                 Expanded(
                   flex: 9,
                   child: HistoryTimelineList(
@@ -52,5 +38,73 @@ class HistoryScreen extends StatelessWidget {
             );
           }),
     );
+  }
+
+  TableCalendar buildTableCalendar(
+      HistoryBloc bloc, List<Record> recordsForRange) {
+    Map<DateTime, List<Record>> _events = Map<DateTime, List<Record>>();
+    for (var record in recordsForRange) {
+      _events[record.timestamp] = List<Record>.of([record]);
+    }
+
+    return TableCalendar(
+      calendarController: _calendarController,
+      onVisibleDaysChanged:
+          (DateTime startDate, DateTime endDate, CalendarFormat format) {
+        print(format);
+        bloc.onVisibleDaysChanged(startDate, endDate);
+      },
+      headerStyle: HeaderStyle(
+        centerHeaderTitle: true,
+        formatButtonVisible: true,
+        formatButtonShowsNext: true,
+      ),
+      calendarStyle: CalendarStyle(
+        selectedColor: Colors.deepOrange[400],
+        todayColor: Colors.deepOrange[200],
+        markersColor: Colors.brown[700],
+        outsideDaysVisible: false,
+      ),
+      events: _events,
+      builders: CalendarBuilders(
+        markersBuilder: (context, date, events, holidays) {
+          final children = <Widget>[];
+          if (events.isNotEmpty) {
+            Record recordForDate = events.first;
+            if (recordForDate != null) {
+              children.add(Container(
+                height: 3.0,
+                width: double.infinity,
+                color: getColor(recordForDate),
+              ));
+              children.add(Text(getEmojiString(recordForDate)));
+            }
+          } else {
+            children.add(Container(
+              height: 3.0,
+              width: double.infinity,
+              color: getColor(null),
+            ));
+          }
+
+          if (events.isNotEmpty) {
+            children.add(
+              Positioned(
+                right: 1,
+                bottom: 1,
+                child: _buildEventsMarker(date, events),
+              ),
+            );
+          }
+
+          return children;
+        },
+      ),
+      endDay: DateTime.now(),
+    );
+  }
+
+  _buildEventsMarker(DateTime date, List events) {
+    return Container();
   }
 }
