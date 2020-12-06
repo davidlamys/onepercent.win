cc68ac448752ef86412017e6424cb017b5a59819
David Lam <davidlys@gmail.com>
2020-12-06 23:33:58 +0800

    app store deployment

--- flutter_win_2/lib/main.dart
+++ flutter_win_2/lib/main.dart
@@ -42,6 +42,7 @@ class MyApp extends StatelessWidget {
       child: NoteEntryProvider(
         child: ReminderProvider(
           child: MaterialApp(
+            debugShowCheckedModeBanner: false,
             home: RouterScreen(),
             onGenerateRoute: routes,
             theme: ThemeData(
